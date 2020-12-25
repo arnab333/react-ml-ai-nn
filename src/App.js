7776fdb17a24aba42d3efb4a3c799eecd25c0c9b
arnab <arnab>
2020-12-25 14:39:43 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useEffect, useState } from 'react';
+
+// import brain from 'brain.js';
+import brain from 'brain.js/src/index';
+import { initialTrainingData } from './helpers/trainingData';
+
+const net = new brain.NeuralNetwork({
+  // hiddenLayers: [4, 5, 6],
+});
+
+const initState = {
+  trainingData: initialTrainingData,
+  randomColor: getRandom(),
+  guessColor: '',
+};
 
 function App() {
+  const [state, setState] = useState(initState);
+
+  function handleState(data) {
+    setState((prevState) => ({
+      ...prevState,
+      ...data,
+    }));
+  }
+
+  useEffect(() => {
+    net.train(state.trainingData);
+    const color = getRandom();
+    const guess = net.run(color)[0];
+
+    handleState({ randomColor: color, guessColor: guess });
+  }, [state.trainingData]);
+
+  function chooseColor(value) {
+    const temp = [...state.trainingData];
+    temp.push({
+      input: state.randomColor,
+      output: [value],
+    });
+
+    handleState({ trainingData: temp });
+  }
+
+  function onWhiteButtonClick() {
+    chooseColor(1);
+  }
+  function onBlackButtonClick() {
+    chooseColor(0);
+  }
+  function onPrintButtonClick() {
+    console.log(JSON.stringify(state.trainingData));
+  }
+
+  // console.log(state.trainingData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div
+        className="color"
+        style={{
+          backgroundColor: `rgba(${state.randomColor.r * 255}, ${
+            state.randomColor.g * 255
+          }, ${state.randomColor.b * 255})`,
+        }}>
+        <div className="white">White Text</div>
+        <div>Black Text</div>
+        <div style={{ color: state.guessColor > 0.5 ? `#FFF` : `#000` }}>
+          Guess Text
+        </div>
+      </div>
+
+      <button onClick={onWhiteButtonClick}>White</button>
+      <button onClick={onBlackButtonClick}>Black</button>
+      <button onClick={onPrintButtonClick}>Print</button>
+    </Fragment>
   );
 }
 
 export default App;
+
+function getRandom() {
+  const color = {
+    r: Math.random(),
+    g: Math.random(),
+    b: Math.random(),
+  };
+
+  return color;
+}
